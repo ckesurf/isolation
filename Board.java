5cbaddc4fdb4c31599f3ec00de57f68c54b076df
Chris Erlendson <cke2106@columbia.edu>
2013-04-05 09:32:48 -0400

    everything seems to work fine

--- Board.java
+++ Board.java
@@ -1,10 +1,12 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 //import java.lang.Math;
 
-
 public class Board {
+	public static int REACH;
+
 	public String[][] board;
 	public int[] x_pos;
 	public int[] o_pos;
@@ -30,6 +32,8 @@ public class Board {
 		o_pos = new int[2];
 		o_pos[0] = 7;
 		o_pos[1] = 7;
+		children = new LinkedList<Board>();
+		REACH = 2;
 
 	}
 
@@ -47,6 +51,9 @@ public class Board {
 		o_pos = new int[2];
 		o_pos[0] = board2.o_pos[0];
 		o_pos[1] = board2.o_pos[1];
+		parent = board2;
+		children = new LinkedList<Board>();
+
 	}
 
 	public void printBoard()
@@ -66,7 +73,7 @@ public class Board {
 		System.out.println();
 	}
 
-	public String opponent(String player)
+	public String opp(String player)
 	{
 		if (player.equals("x"))
 			return "o";
@@ -87,6 +94,10 @@ public class Board {
 		return !isFilled(x, y);
 	}
 
+	/*
+	 * params: 	String player - player
+	 * 			x, y - coordinates, starting at zero (so as normally done)
+	 */
 	public boolean isValid(String player, int x, int y)
 	{
 		/*
@@ -180,14 +191,13 @@ public class Board {
 			// SW?
 			else if (x > r_pos && y < c_pos) {
 				for (int i = 1; i <= c_dist; i++) {
-					if (isFilled(r_pos-i, c_pos+i))
+					if (isFilled(r_pos+i, c_pos-i))
 						return false;
 				}
 				return true; // checked squares NW of "x", no obstructions
 			} 
 
 		}		
-		System.out.println("Neither cardinal nor diagonal");
 		return false; // Neither diagonal nor cardinal
 	}
 
@@ -218,7 +228,7 @@ public class Board {
 
 		for (int i = -1; i < 2; i++) {
 			for (int j = -1; j < 2; j++) {
-				if (i != 0 && j != 0){
+				if (i != 0 || j != 0){
 					if (!outOfBounds(r_pos + i, c_pos + j)) {
 						if (isEmpty(r_pos+i, c_pos+j))
 							return false;
@@ -262,6 +272,13 @@ public class Board {
 			System.out.println("Not valid");
 	}
 
+
+	/*
+	 * If successful, moveNew returns a new Board object with board and 
+	 * positions of players updated appropriately.
+	 * 
+	 * If the move is not valid, moveNew returns null
+	 */
 	public Board moveNew(String player, int x, int y)
 	{
 		x--;
@@ -270,25 +287,10 @@ public class Board {
 		//implement isValid() here!
 		if (isValid(player, x, y)){
 			new_board = new Board(this);
-			if (player.equals("x")) {
-				// replace x's pos with "*"
-				new_board.board[x_pos[0]][x_pos[1]] = "*";
-				// new_board.board now has new position of x
-				new_board.board[x][y] = "x";
-				// update x's registered position
-				new_board.x_pos[0] = x;
-				new_board.x_pos[1] = y;
-			} else if (player.equals("o")){
-				new_board.board[o_pos[0]][o_pos[1]] = "*";
-				new_board.board[x][y] = "o";
-				new_board.o_pos[0] = x;
-				new_board.o_pos[1] = y;
-			} else {
-				System.out.println("error: invalid player");
-			}
-		} else
-			System.out.println("Not valid");
-
+			/* Note! isValid is being run AGAIN in move(...). Optimize this! */
+			// whoops. need to add 1 because in move we decrement it again.
+			new_board.move(player, x+1, y+1);
+		}
 		return new_board;
 	}
 
@@ -309,8 +311,8 @@ public class Board {
 		r_pos++;
 		c_pos++;
 		/* only one move away for now */
-		for (int i = -1; i < 2; i++) {
-			for (int j = -1; j < 2; j++) {
+		for (int i = -REACH; i < REACH+1; i++) {
+			for (int j = -REACH; j < REACH+1; j++) {
 				if (i != 0 || j != 0) {
 					Board b = this.moveNew(player, r_pos+i, c_pos+j);
 					if (b != null)
@@ -321,17 +323,10 @@ public class Board {
 		}
 	}
 
-	public int spaceHeuristic(String player)
+
+	public LinkedList<Board> allMovesGen(String player)
 	{
-		/* base case 1: if player has lost, return negative MAX */
-		if (lose(player))
-			return -Integer.MAX_VALUE;
-		/* base case 2: if opponent has lost, return positive MAX */
-		else if (lose(opponent(player)))
-			return Integer.MAX_VALUE;
-
-
-		int empty_spaces = 0;
+		LinkedList<Board> moves= new LinkedList<Board>();
 		int r_pos, c_pos;
 		if (player.equals("x")) { 	/* "x" location */
 			r_pos = x_pos[0];
@@ -340,33 +335,183 @@ public class Board {
 			r_pos = o_pos[0];
 			c_pos = o_pos[1];
 		}
-		for (int i = -2; i < 3; i++) {
-			for (int j = -2; j < 3; j++) {
-				/* if spot in question is our center (main),
-				 * or is out of bounds, skip
-				 */
-				if ((i == 0 && j == 0) || outOfBounds(r_pos+i, c_pos+j))
-					; 
-				/* else if empty, increment count */
-				else if (isEmpty(r_pos+i, c_pos+j))
-					empty_spaces++;
+		/* increment r_pos and c_pos because we're sending them to moveNew,
+		 * which assumes the parameters are User offset, i.e. starts at 1
+		 */
+		r_pos++;
+		c_pos++;
+		/* only one move away for now */
+		for (int i = -REACH; i < REACH+1; i++) {
+			for (int j = -REACH; j < REACH+1; j++) {
+				if (i != 0 || j != 0) {
+					Board b = this.moveNew(player, r_pos+i, c_pos+j);
+					if (b != null)
+						moves.add(b);
+				}
 			}
+
 		}
-		return empty_spaces;
+		return moves;
 	}
+
+
+
+	public int eval(String player)
+	{
+		/* base case 1: if player has lost, return negative MAX */
+		if (lose(player))
+			return -Integer.MAX_VALUE;
+		/* base case 2: if opp has lost, return positive MAX */
+		else if (lose(opp(player)))
+			return Integer.MAX_VALUE;
+		else {
+
+
+			int empty_spaces = 0;
+			int r_pos, c_pos;
+			if (player.equals("x")) { 	/* "x" location */
+				r_pos = x_pos[0];
+				c_pos = x_pos[1];
+			} else {					/* "o" location */
+				r_pos = o_pos[0];
+				c_pos = o_pos[1];
+			}
+			for (int i = -2; i < 3; i++) {
+				for (int j = -2; j < 3; j++) {
+					/* if spot in question is our center (main),
+					 * or is out of bounds, skip
+					 */
+					if ((i == 0 && j == 0) || outOfBounds(r_pos+i, c_pos+j))
+						; 
+					/* else if empty, increment count */
+					else if (isEmpty(r_pos+i, c_pos+j))
+						empty_spaces++;
+				}
+			}
+			
+			return empty_spaces;
+		}
+	}
+
+
+
+
+
+	public Pair<Integer, Board> BESTMOVE(int depth, int MyBest, int HerBest, String player)
+	{
+
+		Pair<Integer, Board> p;
+
+		if (depth == 0) {
+			p = new Pair<Integer, Board>(eval(player), null);
+			return p;
+		} else if (lose(player)) {
+			p = new Pair<Integer, Board>(-Integer.MAX_VALUE, this);
+			return p;
+			/* base case 2: if opp has lost, return positive MAX */
+		} else if (lose(opp(player))) {
+			p = new Pair<Integer, Board>(Integer.MAX_VALUE, this);
+			return p;
+		} else {
+
+			LinkedList<Board> move_list = allMovesGen(player);
+
+			int best_score = MyBest;
+			Board best_move = null;
+
+			while (move_list.size() > 0) {
+
+				Pair<Integer, Board> Try = move_list.getFirst().BESTMOVE(depth-1, -HerBest, -MyBest, opp(player));
+				int tryscore = -Try.getL();
+				if (tryscore >= best_score) {
+					best_score = tryscore;
+					best_move = move_list.getFirst();
+				}
+
+				if (best_score > HerBest) {
+					p = new Pair<Integer, Board>(best_score, best_move); // alpha-beta pruning here
+					return p;
+				}
+
+				move_list.removeFirst();
+
+			} // out of while
+			
+			p = new Pair<Integer, Board>(best_score, best_move);
+			return p;
+		} // end of proc
+	}
+
+
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Board asdf = new Board();
-		//asdf.move("x", 4, 4);
+		System.out.println("Enter your player: ");
+		Scanner scanner = new Scanner( System.in );
+		String player = scanner.nextLine(); 
+		Pair<Integer, Board> p;
+		// set x up to lose
+//		asdf.move("x", 2, 1);
+//		asdf.move("x", 2, 2);
+//		asdf.move("x", 2, 3);
+//		asdf.move("x", 2, 4);
+//		asdf.move("x", 1, 4);
+//		asdf.move("x", 1, 3);
 		asdf.printBoard();
-
-		asdf.generateMoves("x");
-
-		for (Board child : asdf.children)
-			child.printBoard();
+		
+		
+		if (player.equals("x")) {
+			
+			System.out.println("Enter row: ");
+			int row = scanner.nextInt();
+			System.out.println("Enter col:");
+			int col = scanner.nextInt();
+			while(!asdf.isValid(player, row-1, col-1)){
+				System.out.println("Not a valid move, please try again");
+				System.out.println("Enter row: ");
+				row = scanner.nextInt();
+				System.out.println("Enter col:");
+				col = scanner.nextInt();
+			}
+			asdf.move(player, row, col);
+			asdf.printBoard();
+		}
+		
+		while (true){ 
+			/* start the game: if player was o, computer, or "x" will go first 
+			 * Remember, you are player, computer is always opp(player)
+			 */
+			p = asdf.BESTMOVE(5, -Integer.MAX_VALUE, Integer.MAX_VALUE, asdf.opp(player));
+			asdf = p.getR();
+			asdf.printBoard();
+			
+			if (asdf.lose(asdf.opp(player))) {
+				System.out.println(player + " wins!");
+				return;
+			}
+			/* then your turn */
+			System.out.println("Enter row: ");
+			int row = scanner.nextInt();
+			System.out.println("Enter col:");			
+			int col = scanner.nextInt();
+			while(!asdf.isValid(player, row-1, col-1)){
+				System.out.println("Not a valid move, please try again");
+				System.out.println("Enter row: ");
+				row = scanner.nextInt();
+				System.out.println("Enter col:");
+				col = scanner.nextInt();
+			}
+			asdf.move(player, row, col);
+			asdf.printBoard();
+			if (asdf.lose(player)) {
+				System.out.println(asdf.opp(player) + " wins!");
+				return;
+			}
+		}
 
 	}
 
